22cdcad90773929f47ea3629bd88b210d97f1aca
Naposetivi <110626741+Naposetivi@users.noreply.github.com>
2024-04-02 12:29:12 +0300

    not fully done
    But I manau to do it

---
+++ PR-8/scripts.js
@@ -0,0 +1,13 @@
+
+var names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i = 0; i < names.length; i++) {
+	var firstLetter = names[i].charAt(0).toLowerCase();
+
+	if (firstLetter === 'j') {
+		speakGoodBye.speak(names[i]);
+	} else {
+		speakHello.speak(names[i]);
+	}
+}
+
