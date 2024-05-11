65c621dc47af6f0b3b662cbc5aa09e6bd96248f7
Naposetivi <110626741+Naposetivi@users.noreply.github.com>
2024-05-11 19:01:52 +0200

    Again

--- CR-3/script.js
+++ CR-3/script.js
@@ -2,7 +2,7 @@ function calculate(operatia) {
 	var op1 = parseFloat(document.getElementById("op1").value);
 	var op2 = parseFloat(document.getElementById("op2").value);
 	var result;
-	if (!op1 && operatia == "log" || operatia == "sin" || operatia == "tan") {
+	if (!op1 && operatia == "log-button" || operatia == "sin-button" || operatia == "tan-button") {
 		result = "You must enter first operand(";
 	}
 	else if (!op1 || !op2) {
